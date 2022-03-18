542bac6402baafc2dd905b7d80c1bf460af7f200
Dennis Akagha <d.akagha20@gmail.com>
2022-03-18 19:57:05 +0100

    Routing

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -5,14 +5,21 @@ import { useState, useEffect } from 'react';
 
 const Wrapper = styled.div`
     display: grid;
-    grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(8, 5vw);
-    /* grid-gap: 1.5rem; */
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 5rem;
     
 `
 
 const Container = styled.div`
-    height: 15rem;
+    height: 5rem;
+    width: 2.5rem;
+    gap: 10rem;
+
+    img{
+        width: 12rem;
+        /* height: 100%; */
+    }
     
 
 `
@@ -23,7 +30,7 @@ const RecipeList = () => {
     const fetchRecipe = async () => {
         const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&q=%22chicken%22%2C%20%22salad%22&app_id=5abb86d8&app_key=05efb931f11c66c1391a6bd02f9438d9');
         const data = await response.json()
-        // setRecipeList(data.hits);
+        setRecipeList(data.hits);
         console.log(data.hits);
     }
 
@@ -31,15 +38,14 @@ const RecipeList = () => {
         fetchRecipe();
     }, [])
   return (
-    <>
-        {recipeList.map((list) => {
-            return (
-                <>
-                {list.recipe.label}
-                </>
-            )
+    <Wrapper>
+        {recipeList.map((recipe) => {
+            return <Container>
+                <img src={recipe.recipe.image} alt="" />
+            </Container>
+
         })}
-    </>
+    </Wrapper>
   )
 }
 
